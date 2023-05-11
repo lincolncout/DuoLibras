0c91a8a1bc9dc350aacfc76a0395a24ace899a06
Student02 <student02@HackaTruck02.local>
2023-05-11 19:25:11 -0300

    tela de tema finalizado

--- DuoLibras/DuoLibras/Views/FinishedTema.swift
+++ DuoLibras/DuoLibras/Views/FinishedTema.swift
@@ -3,17 +3,38 @@ import SwiftUI
 struct FinishedTema: View {
     var acertos: Int
     
-    var message: String = ""
+    @State var titulo: String = ""
+    @State var message: String = ""
     
     var body: some View {
         VStack {
             
-            Text("Título")
-            Text("Textinho.. Você acertou \(acertos) pergunta(s)")
+            Text(titulo)
+                .font(.title)
+                .foregroundColor(.white)
+                .padding()
             
+            Text(message).padding().foregroundColor(.white)
+                        
             NavigationLink(destination: ContentView().navigationBarBackButtonHidden(true)) {
-                Text("Voltar para Home").foregroundColor(Color.white).frame(width: 200, height: 50).background(Color.purple.opacity(0.5)).cornerRadius(20)
+                Text("Voltar para Home").foregroundColor(Color.white).frame(width: 200, height: 50).background(Color.pink.opacity(0.4)).cornerRadius(20)
             }.isDetailLink(false)
+        }.frame(width: 300, height: 350, alignment: .center).background(Color.purple.opacity(0.6)).cornerRadius(20)
+        .onAppear() {
+            controlMessage()
+        }
+    }
+    
+    func controlMessage() {
+        if acertos == 0 {
+            self.titulo = "Que pena..."
+            self.message = "Parece que você não acertou nenhuma pergunta. Mas fique tranquilo, você pode refazer o módulo sempre que quiser."
+        } else if acertos >= 1 && acertos <= 3 {
+            self.titulo = "Bom trabalho"
+            self.message = "Você acertou \(acertos) pergunta(s). Continue trabalhando duro no aprendizado de Libras. Lembre-se, você pode refazer o módulo sempre que quiser"
+        } else if acertos == 4 {
+            self.titulo = "Parabéns!"
+            self.message = "Você acertou todas as questões deste módulo! Daqui a pouco está fluente!"
         }
     }
 }
